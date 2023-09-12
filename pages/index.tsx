1eba802e6cf957610e57ea10b1bb9e56d3a5c1d7
Juan Pablo Bedoya <bedoya.pablo.7@gmail.com>
2023-09-12 18:13:26 -0500

    crear inicio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,71 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { FormEvent, useState } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
-  return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+interface FormDataInterface {
+    [key: string] : string;
 }
+
+const index = () => {
+    const submitForm = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log('Los datos del usuario son: ', formData);
+    }
+    // const [user, setUser] = useState<string>('');
+    // const [password, setPassword] = useState<string>('');
+    const [formData, setFormData] = useState<FormDataInterface>({user: '', password: ''});
+  return (
+    <>
+        <div className='flex'>
+            <div className='w-2/5 h-screen flex flex-col py-12 pl-12 justify-between'>
+                <div className='flex flex-col'>
+                    {/* <span className='Green-Hard text-[64px] font-bold'>SignAI</span> */}
+                    <span className='Green-Hard text-[64px] font-bold'>UdeAcercate</span>
+                    {/* <span className='Green-Mid text-xl'>UNIENDO MUNDOS SEÑA POR SEÑA</span> */}
+                    <span className='Green-Mid text-xl'>!LA RED SOCIAL PARA EVENTO CULTURALES, ACADEMICOS, Y DEPORTIVOS EN LA UDEA!</span>
+                </div>
+                <div className=''>
+                    <span className='Light-Grey text-xl'>Contáctanos</span>
+                </div>
+            </div>
+            <div className='w-3/5 h-screen background-form flex justify-center items-center'>
+                <form onSubmit={submitForm}>
+                    <div className='w-[280px] h-[380px]  pt-12 px-6 rounded-xl flex flex-col gap-8 bg-white shadow-2xl'>
+                        <h1 className='text-center Green-Hard font-bold text-[32px]'>Iniciar Sesión</h1>
+                        <label htmlFor="user">
+                            <span className='Light-Grey font-medium text-base'>Usuario</span>
+                            <input
+                            className='input-form' 
+                            type="text"
+                            name='user'
+                            value={formData.user}
+                            onChange={(e)=>{
+                                setFormData({...formData, [e.target.name]: e.target.value});
+                            }}
+                            required
+                            />
+                        </label>
+                        <label htmlFor="password">
+                            <span className='Light-Grey font-medium text-base'>Contraseña</span>
+                            <input
+                            className='input-form' 
+                            type="password"
+                            name='password'
+                            value={formData.password}
+                            onChange={(e) => {
+                                setFormData({...formData, [e.target.name]: e.target.value});
+                            }}
+                            required
+                            />
+                        </label>
+                        <button type='submit' className='button-form shadow-lg text-white font-bold text-base'>Ingresar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div className='logo-container'>
+            <img src="/logo-udea.png" alt="logo udea"/>
+        </div>
+    </>
+    )
+}
+
+export default index;
