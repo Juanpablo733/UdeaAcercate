6a6fa6f06e8b9f44f13586672b06289ec7928c0a
JoseMagg <jose.gonzalezg1@udea.edu.co>
2023-09-20 08:11:00 -0500

    patch perfil y delete perfil y usuario

--- models/usuarioModel.ts
+++ models/usuarioModel.ts
@@ -4,22 +4,23 @@ import { PerfilModel, perfil } from "./perfilModel";
 interface usuario {
     correo: string,
     nombre: string,
-    perfil: typeof PerfilModel
+    // perfil: typeof PerfilModel
 }
 
 const usuarioSchema = new mongoose.Schema<usuario>({
     correo: {
         type: String,
+        unique: true,
         required: true,
     },
     nombre: {
         type: String,
         required: true,
     },
-    perfil: {
-        type: PerfilModel,
-        required: true,
-    }
+    // perfil: {
+    //     type: PerfilModel,
+    //     required: true,
+    // }
 });
 
 const UsuarioModel = mongoose.models.Usuario || mongoose.model("Usuario", usuarioSchema);
