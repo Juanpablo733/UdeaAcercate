d0df60235196b361517b48961387afd9dfccf30f
santiago1023 <santiago.bedoyad@udea.edu.co>
2023-09-19 22:47:57 -0500

    Se agrega el navbar, y se hace un bosquejo de como se quiere ver el home

---
+++ components/card/Card.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='w-[500px] h-[700px] debug '>Card</div>
+  )
+}
+
+export {Card}
---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import React from 'react'
+
+interface NavbarProps {
+    children: JSX.Element;
+}
+
+const Navbar = ({children}: NavbarProps) => {
+  return (
+    <div className='w-screen h-16 debug bg-[#026937] flex justify-between'>
+        <div className='debug flex items-center'>
+            <Image src={'/udea-texto.png'} alt={'udea-texto'} width={100} height={64} className='debug'/>
+            <Image src={'/cercate-texto.png'} alt={'cercate-texto'} width={100} height={64} className='debug'/>
+        </div>
+        <div className='debug'>
+            {children}
+        </div>
+    </div>
+  )
+}
+
+export {Navbar};
