4f494e66acf9dc9364dbabd3ef303252b1fce88d
JoseMagg <jose.gonzalezg1@udea.edu.co>
2023-11-07 10:19:38 -0500

    merge upstream

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,13 +17,16 @@ const index = () => {
     const [formData, setFormData] = useState<FormDataInterface>({user: '', password: ''});
   return (
     <>
+    <title>
+        UdeAcercate
+    </title>
         <div className='flex'>
             <div className='w-2/5 h-screen flex flex-col py-12 pl-12 justify-between'>
                 <div className='flex flex-col'>
                     {/* <span className='Green-Hard text-[64px] font-bold'>SignAI</span> */}
                     <span className='Green-Hard text-[64px] font-bold'>UdeAcercate</span>
                     {/* <span className='Green-Mid text-xl'>UNIENDO MUNDOS SEÑA POR SEÑA</span> */}
-                    <span className='Green-Mid text-xl'>!LA RED SOCIAL PARA EVENTO CULTURALES, ACADEMICOS, Y DEPORTIVOS EN LA UDEA!</span>
+                    <span className='Green-Mid text-xl'>¡LA RED SOCIAL PARA EVENTOS CULTURALES, ACADÉMICOS, Y DEPORTIVOS EN LA UDEA!</span>
                 </div>
                 <div className=''>
                     <span className='Light-Grey text-xl'>Contáctanos</span>
@@ -31,7 +34,7 @@ const index = () => {
             </div>
             <div className='w-3/5 h-screen background-form flex justify-center items-center'>
                     <div className='w-[280px] h-[380px]  pt-12 px-6 rounded-xl flex flex-col justify-around bg-white shadow-2xl'>
-                        <h1 className='text-center Green-Hard font-bold text-[32px]'>Solo puedes ingresar con correos @udea.edu.co</h1>      
+                        <h1 className='text-center Green-Hard font-bold text-[32px]'>Sólo puedes ingresar con correos @udea.edu.co</h1>      
                         <button type='submit' className='button-form shadow-lg text-white font-bold text-base' onClick={LoginWithGoogle}>
                             Ingresar con correo UdeA
                             </button>
