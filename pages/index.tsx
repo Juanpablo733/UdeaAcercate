ce15952687b308617bbc58d68fa5815dbbb0f5cf
Juanpablo733 <bedoya.pablo.7@gmail.com>
2023-11-06 22:24:32 -0500

    cambiar el inicio sesion

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,9 +17,6 @@ const index = () => {
     const [formData, setFormData] = useState<FormDataInterface>({user: '', password: ''});
   return (
     <>
-    <Button onClick={LoginWithGoogle}>
-        Iniciar sesion con Google
-    </Button>
         <div className='flex'>
             <div className='w-2/5 h-screen flex flex-col py-12 pl-12 justify-between'>
                 <div className='flex flex-col'>
@@ -33,38 +30,12 @@ const index = () => {
                 </div>
             </div>
             <div className='w-3/5 h-screen background-form flex justify-center items-center'>
-                <form onSubmit={submitForm}>
-                    <div className='w-[280px] h-[380px]  pt-12 px-6 rounded-xl flex flex-col gap-8 bg-white shadow-2xl'>
-                        <h1 className='text-center Green-Hard font-bold text-[32px]'>Iniciar Sesión</h1>
-                        <label htmlFor="user">
-                            <span className='Light-Grey font-medium text-base'>Usuario</span>
-                            <input
-                            className='input-form' 
-                            type="text"
-                            name='user'
-                            value={formData.user}
-                            onChange={(e)=>{
-                                setFormData({...formData, [e.target.name]: e.target.value});
-                            }}
-                            required
-                            />
-                        </label>
-                        <label htmlFor="password">
-                            <span className='Light-Grey font-medium text-base'>Contraseña</span>
-                            <input
-                            className='input-form' 
-                            type="password"
-                            name='password'
-                            value={formData.password}
-                            onChange={(e) => {
-                                setFormData({...formData, [e.target.name]: e.target.value});
-                            }}
-                            required
-                            />
-                        </label>
-                        <button type='submit' className='button-form shadow-lg text-white font-bold text-base'>Ingresar</button>
+                    <div className='w-[280px] h-[380px]  pt-12 px-6 rounded-xl flex flex-col justify-around bg-white shadow-2xl'>
+                        <h1 className='text-center Green-Hard font-bold text-[32px]'>Solo puedes ingresar con correos @udea.edu.co</h1>      
+                        <button type='submit' className='button-form shadow-lg text-white font-bold text-base' onClick={LoginWithGoogle}>
+                            Ingresar con correo UdeA
+                            </button>
                     </div>
-                </form>
             </div>
         </div>
         <div className='logo-container'>
