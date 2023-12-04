feebe21b1a4238704b7017f8de9da89a0752390b
santiago1023 <santiago.bedoyad@udea.edu.co>
2023-12-03 19:41:54 -0500

    Se suben los mensajes con toastify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import PrivateLayout from '@/layouts/PrivateLayout'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const client = new ApolloClient({
@@ -11,10 +13,13 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
   })
 
   return (
+    <>
     <ApolloProvider client={client}>
       <SessionProvider session={session}>
           <Component {...pageProps} />
       </SessionProvider>
     </ApolloProvider>
+    <ToastContainer/>
+    </>
   )
 }
