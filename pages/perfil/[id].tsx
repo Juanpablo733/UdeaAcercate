bec01f5ebf1515cd1495704f141600c5eb0326d0
JoseMagg <jose.gonzalezg1@udea.edu.co>
2023-12-04 08:40:38 -0500

    titulos

--- pages/perfil/[id].tsx
+++ pages/perfil/[id].tsx
@@ -33,11 +33,11 @@ const Perfil = () => {
     const user = profile?.user
     const setCreatedType = async () => {
         await setEventType('created')
-        console.log('Tipo de evento',eventType)
+        console.log('Tipo de evento', eventType)
     }
     const setAttendingType = async () => {
         await setEventType('attending')
-        console.log('Tipo de evento',eventType)
+        console.log('Tipo de evento', eventType)
     }
 
     console.log("Profile:", profileData)
@@ -45,58 +45,68 @@ const Perfil = () => {
     console.log("errorCreated:", errorCreated)
     console.log("eventsAttending:", eventsAttendingData)
     return (
+        <>
+            <title>
+                Perfil | UdeAcercate
+            </title>
+            <div className='Yellow-little gap-15 flex flex-col h-screen'>
+                <div>
+                    <Navbar>
+                        <Link href={'/home'}>
+                            <div className='h-full flex items-center justify-center'>
+                                <MdHomeFilled className="h-9 w-9 mr-20" />
+                            </div>
+                        </Link>
+                    </Navbar>
+                </div>
 
-        <div className='Yellow-little gap-15 flex flex-col h-screen'>
-            <div>
-                <Navbar>
-                    <Link href={'/home'}>
-                        <div className='h-full flex items-center justify-center'>
-                            <MdHomeFilled className="h-9 w-9 mr-20" />
+                <div className=' h-full flex flex-row'>
+                    <div className=' w-1/3 flex flex-col items-center p-4 gap-4'>
+                        <Image src={user?.image!} alt={'avatar-image'} height={200} width={200} className=' rounded-full' />
+                        <span className='text-xl font-extrabold'>{user?.name}</span>
+                        <span className='text-sm font-bold'>{profile?.faculty}</span>
+                        <span className='text-sm font-bold'>{profile?.career}</span>
+                        <span className='text-sm font-bold'>{profile?.campus?.replace('_', ' ')}</span>
+                        <span className='text-sm font-bold'>{profile?.type}</span>
+                        <span className='text-sm font-bold mr-7 ml-7'>
+                            {profile?.description}
+                        </span>
+                        <span className='text-sm font-bold mr-7 ml-7'>
+                            {profile?.hobbies}
+                        </span>
+                        <span className='text-sm font-bold mr-7 ml-7'>
+                            {profile?.socialLinks}
+                        </span>
+                    </div>
+                    <div className=' w-full'>
+                        <div className=' w-full p-4 grid grid-cols-2 gap-4 justify-items-center'>
+                            <button className=
+                                {eventType === 'created'
+                                    ? ' ButtonCard  flex items-center h-14 w-60 justify-center'
+                                    : ' border-2 flex items-center h-14 w-60 justify-center'}
+                                onClick={setCreatedType}>
+                                Eventos Creados
+                            </button>
+                            <button className=
+                                {eventType === 'attending'
+                                    ? ' ButtonCard  flex items-center h-14 w-60 justify-center'
+                                    : ' border-2 flex items-center h-14 w-60 justify-center'}
+                                onClick={setAttendingType}>
+                                Eventos a asistir
+                            </button>
                         </div>
-                    </Link>
-                </Navbar>
-            </div>
-
-            <div className='debug h-full flex flex-row'>
-                <div className='debug w-1/3 flex flex-col items-center p-4 gap-4'>
-                    <Image src={user?.image!} alt={'avatar-image'} height={200} width={200} className='debug rounded-full' />
-                    <span className='text-xl font-extrabold'>{user?.name}</span>
-                    <span className='text-sm font-bold'>{profile?.faculty}</span>
-                    <span className='text-sm font-bold'>{profile?.career}</span>
-                    <span className='text-sm font-bold'>{profile?.campus?.replace('_', ' ')}</span>
-                    <span className='text-sm font-bold'>{profile?.type}</span>
-                    <span className='text-sm font-bold mr-7 ml-7'>
-                        {profile?.description}
-                    </span>
-                    <span className='text-sm font-bold mr-7 ml-7'>
-                        {profile?.hobbies}
-                    </span>
-                    <span className='text-sm font-bold mr-7 ml-7'>
-                        {profile?.socialLinks}
-                    </span>
-                </div>
-                <div className='debug w-full'>
-                    <div className='debug w-full p-4 grid grid-cols-2 gap-4 justify-items-center'>
-                        <button className='debug ButtonCard  flex items-center h-14 w-60 justify-center'
-                            onClick={setCreatedType}>
-                            Eventos Creados
-                        </button>
-                        <button className='debug ButtonCard  flex items-center h-14 w-60 justify-center'
-                            onClick={setAttendingType}>
-                            Eventos a asistir
-                        </button>
-                    </div>
-                    <div>
-                        <MiniCardContainer data=
-                            {eventType === 'created'
-                                ? eventsCreatedData?.eventsCreated
-                                : eventsAttendingData?.eventsAttending}
-                        />
+                        <div>
+                            <MiniCardContainer data=
+                                {eventType === 'created'
+                                    ? eventsCreatedData?.eventsCreated
+                                    : eventsAttendingData?.eventsAttending}
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
 
-        </div>
+            </div>
+        </>
     )
 }
 
--- pages/signInError.tsx
+++ pages/signInError.tsx
@@ -3,15 +3,20 @@ import { signIn } from 'next-auth/react';
 import React from 'react';
 
 const SignInError = () => {
-    const LoginWithGoogle = () => {signIn('google', {callbackUrl: '/home'})}
+    const LoginWithGoogle = () => { signIn('google', { callbackUrl: '/home' }) }
     return (
-        <div className='h-screen flex flex-col items-center justify-center bg-[var(--green-hard)] gap-4'>
-            <p className='text-white font-bold text-2xl'>Error de inicio de sesión</p>
-            <h2 className='text-white font-bold text-2xl'>Por favor inicie sesión con una cuenta institucional terminada en @udea.edu.co</h2>
-            <Button onClick={LoginWithGoogle} className='text-[var(--green-hard)] bg-white font-bold text-lg hover:bg-gray-300'>
-                Volver a intentar
-            </Button>
-        </div>
+        <>
+            <title>
+                Error de inicio de sesión | UdeAcercate
+            </title>
+            <div className='h-screen flex flex-col items-center justify-center bg-[var(--green-hard)] gap-4'>
+                <p className='text-white font-bold text-2xl'>Error de inicio de sesión</p>
+                <h2 className='text-white font-bold text-2xl'>Por favor inicie sesión con una cuenta institucional terminada en @udea.edu.co</h2>
+                <Button onClick={LoginWithGoogle} className='text-[var(--green-hard)] bg-white font-bold text-lg hover:bg-gray-300'>
+                    Volver a intentar
+                </Button>
+            </div>
+        </>
     )
 }
 
