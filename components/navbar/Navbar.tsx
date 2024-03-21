291b2fef5c38ec661c80c9a92e71c7eb83721e54
santiago1023 <santiago.bedoyad@udea.edu.co>
2024-03-21 10:07:57 -0500

    Se trabaja en el diseño responsive del Navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -3,28 +3,32 @@ import Image from 'next/image';
 import React from 'react'
 import { signOut } from 'next-auth/react';
 import Link from 'next/link';
+import { useUserData } from '@/hooks/useUserData';
+import { MdDehaze, MdKeyboardBackspace } from "react-icons/md";
 
-
-interface NavbarProps {
-    children: JSX.Element;
-}
-
-const Navbar = ({children}: NavbarProps) => {
+const Navbar = () => {
+  const { loading: loadingUser, session, status, userData } = useUserData();
+  const userId = userData?.user.id
   const CloseSession = () => { signOut({ callbackUrl: '/' }) }  
   return (
-    <div className=' top-0 left-0 w-full h-16 bg-[#026937] flex justify-around items-center'>
-      <button className='text-white font-bold' onClick={CloseSession} >
-                Cerrar sesión
+    <div className='w-full h-16 bg-[#026937] flex items-center justify-between debug px-4'>
+      <button className='text-white font-bold hidden sm:block' onClick={CloseSession} >
+        Cerrar sesión
       </button>
-        <Link href={'/home'} >
-          <div className=' flex items-center'>
-              <Image src={'/udea-texto.png'} alt={'udea-texto'} width={100} height={64} className=''/>
-              <Image src={'/cercate-texto.png'} alt={'cercate-texto'} width={100} height={64} className=''/>
-          </div>
-        </Link>
-        <div className=''>
-            {children}
+      <MdKeyboardBackspace className='h-8 w-8 sm:hidden'/>
+      <Link href={'/home'} >
+        <div className=' flex items-center'>
+            <span className='text-3xl font-bold text-white font-mono '>UdeA</span>
+            <span className='text-xl text-white font-serif'>cércate</span>
         </div>
+      </Link>
+      <Link href={`/perfil/${userId}`}>
+          <div className='gap-4 items-center justify-center hidden sm:flex'>
+              <span className=' text-white font-bold'>{session?.user?.name}</span>
+              <Image src={session?.user?.image!} alt={'avatar-image'} height={50} width={50} className='rounded-full' />
+          </div>
+      </Link>
+      <MdDehaze className='h-8 w-8 sm:hidden'/>
     </div>
   )
 }
