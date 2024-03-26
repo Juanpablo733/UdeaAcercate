0de5940a015a788e73024572ca3474821c566113
santiago1023 <santiago.bedoyad@udea.edu.co>
2024-03-26 18:49:24 -0500

    Se adelanta el diseño responsive del login de la pagina, quedan pendiente ajustar algunas cosas de CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
+import LoginDesktop from '@/components/login/LoginDesktop';
+import LoginMobile from '@/components/login/LoginMobile';
 import { signIn } from 'next-auth/react';
-import Image from 'next/image';
 import React, { FormEvent, useState } from 'react'
 
 interface FormDataInterface {
@@ -15,35 +16,14 @@ const Index = () => {
     // const [user, setUser] = useState<string>('');
     // const [password, setPassword] = useState<string>('');
     const [formData, setFormData] = useState<FormDataInterface>({user: '', password: ''});
+    
   return (
     <>
-    <title>
-        UdeAcercate
-    </title>
-        <div className='flex'>
-            <div className='w-2/5 h-screen flex flex-col py-12 pl-12 justify-between'>
-                <div className='flex flex-col'>
-                    {/* <span className='Green-Hard text-[64px] font-bold'>SignAI</span> */}
-                    <span className='Green-Hard text-[64px] font-bold'>UdeAcercate</span>
-                    {/* <span className='Green-Mid text-xl'>UNIENDO MUNDOS SEÑA POR SEÑA</span> */}
-                    <span className='Green-Mid text-xl'>¡LA RED SOCIAL PARA EVENTOS CULTURALES, ACADÉMICOS, Y DEPORTIVOS EN LA UDEA!</span>
-                </div>
-                <div className=''>
-                    <span className='Light-Grey text-xl'>Contáctanos</span>
-                </div>
-            </div>
-            <div className='w-3/5 h-screen background-form flex justify-center items-center'>
-                    <div className='w-[280px] h-[380px]  pt-12 px-6 rounded-xl flex flex-col justify-around bg-white shadow-2xl'>
-                        <h1 className='text-center Green-Hard font-bold text-[32px]'>Sólo puedes ingresar con correos @udea.edu.co</h1>      
-                        <button type='submit' className='button-form shadow-lg text-white font-bold text-base' onClick={LoginWithGoogle}>
-                            Ingresar con correo UdeA
-                            </button>
-                    </div>
-            </div>
-        </div>
-        <div className='logo-container'>
-            <Image src="/logo-udea.png" alt="logo udea" width={150} height={100}/>
-        </div>
+        <title>
+            UdeAcercate
+        </title>
+        <LoginDesktop LoginWithGoogle={LoginWithGoogle}/>
+        <LoginMobile LoginWithGoogle={LoginWithGoogle}/>
     </>
     )
 }
